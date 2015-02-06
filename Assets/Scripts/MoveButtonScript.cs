e82e2db68ab60bd824a87950f183a45cfe9ce3a0
jscotty <jscotty@hotmail.nl>
2015-02-06 11:19:52 +0100

    character movement

---
+++ Assets/Scripts/MoveButtonScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveButtonScript : MonoBehaviour {
+
+	public float x,y;
+
+	GameObject player;
+
+	PlayerController playerController;
+
+	void Start (){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerController = player.GetComponent<PlayerController> ();
+	}
+
+
+	public void onDown(string direction){
+		if (direction == "RIGHT") {
+			playerController.x = 1f;
+			playerController.y = 0f;
+		} else if (direction == "LEFT") {
+			playerController.x = -1f;
+			playerController.y = 0;
+		} else if (direction == "UP") {
+			playerController.x = 0;
+			playerController.y = 1f;
+		} else if (direction == "DOWN") {
+			playerController.x = 0;
+			playerController.y = -1f;
+		}
+	}
+
+	public void onUP(){
+		playerController.x = 0f;
+		playerController.y = 0f;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,35 +4,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 2f;
-	//public float rotSpeed = 2f;
-
-	GameObject joystick;
-	Vector3 player;
-
-	float xAxis;
-	float yAxis;
-	float rot;
+	
+	public float x,y;
+	public GameObject[] joystick;
 
 	void Start(){
-		joystick = GameObject.FindGameObjectWithTag ("Joystick");
-		player = transform.position;
+
 	}
 
 	void Update () {
-		xAxis = joystick.GetComponent<Joystick> ().jXaxis;
-		yAxis = joystick.GetComponent<Joystick> ().jYaxis;
-
-		player.x += xAxis * speed * Time.deltaTime;
-		player.z += yAxis * speed * Time.deltaTime;
-
-		rot = (player.x * player.z + 90);
-
-		transform.Translate (new Vector2 (xAxis,  yAxis) * speed * Time.deltaTime);
-
-		//transform.position = player;
-
-
-		//print (rot);
+		transform.Translate (new Vector2 (x, y) * speed * Time.deltaTime);
 
 	}
 }
