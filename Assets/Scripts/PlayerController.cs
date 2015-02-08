a7d36fbee1eddebcd192400f4c5a0a57e3c1e450
jscotty <jscotty@hotmail.nl>
2015-02-08 14:07:22 +0100

    added better charactermovement with velocity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,41 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 2f;
+	public float speed = 12f;
 	
 	public float x,y;
-	public GameObject[] joystick;
+	Vector2 player;
+
+	Rigidbody2D body;
+	Transform trans;
 
 	void Start(){
-
+		body = rigidbody2D;
+		trans = transform;
 	}
 
-	void Update () {
-		transform.Translate (new Vector2 (x, y) * speed * Time.deltaTime);
+	void FixedUpdate () {
+		// moving with Velocity for an stable collision ( without shake bounding )
+		Vector2 moveVelocity = body.velocity;
+		moveVelocity.x = x * speed;
+		moveVelocity.y = y * speed;
+		body.velocity = moveVelocity;
+	}
 
+	void Update(){
+		//rotating player:
+		Rotate ();
+	}
+
+	private void Rotate(){
+		if (x > 0f && y == 0f) {
+			transform.eulerAngles = new Vector3(0f, 0f,270f);
+		} else if (x < 0f && y == 0f) {
+			transform.eulerAngles = new Vector3(0f, 0f, 90f);
+		} else if (x == 0f && y > 0f) {
+			transform.eulerAngles = new Vector3(0f, 0f, 0f);
+		} else if (x == 0f && y < 0f) {
+			transform.eulerAngles = new Vector3(0f, 0f, 180f);
+		}
 	}
 }
---
+++ Assets/Scripts/data/DataClass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+public class DataClass : MonoBehaviour {
+
+	string savePath;
+
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+		savePath = Application.persistentDataPath;
+		print (savePath);
+	}
+}
